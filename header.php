29abad09da4e4b5d8844eb61303b2c264e48fd69
salekelfahim <salekelfahim@gmail.com>
2025-04-24 15:17:39 +0100

    adding favoris page

--- header.php
+++ header.php
@@ -17,12 +17,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   </head>
   <body <?php body_class(); ?>>
-    <nav class="navbar <?php echo is_front_page() || is_page('fruits') || is_page('legumes') ? 'home-navbar' : ''; ?>">
+    <nav class="navbar <?php echo is_front_page() || is_page('fruits') || is_page('legumes') || is_page('favoris') ? 'home-navbar' : ''; ?>">
       <div class="container-fluid">
         <div class="row align-items-center w-100">
           <div class="col-md-3">
             <div class="logo">
-              <?php if (is_front_page() || is_page('fruits') || is_page('legumes')): ?>
+              <?php if (is_front_page() || is_page('fruits') || is_page('legumes') || is_page('favoris')): ?>
                 <?php echo get_custom_logo(); ?>
               <?php else: ?>
                 <img src="http://localhost/wordpress/wp-content/uploads/2025/04/logo-2.png" alt="Le Grand Marché Logo" />
@@ -40,9 +40,9 @@
             );
             ?>
           </div>
-          <?php if (is_front_page() || is_page('fruits') || is_page('legumes')): ?>
+          <?php if (is_front_page() || is_page('fruits') || is_page('legumes') || is_page('favoris')): ?>
           <div class="<?php echo is_user_logged_in() ? 'col-md-1' : 'col-md-3'; ?>">
-            <?php if (is_front_page() || is_page('fruits') || is_page('legumes')) : ?>
+            <?php if (is_front_page() || is_page('fruits') || is_page('legumes') || is_page('favoris')) : ?>
               <?php if (!is_user_logged_in()) : ?>
                 <div class="auth-buttons">
                   <a href="<?php echo get_permalink(get_page_by_path('connexion')); ?>" class="btn btn-outline">Connexion</a>
@@ -50,7 +50,7 @@
                 </div>
               <?php else : ?>
                 <div class="user-icons d-flex align-items-center justify-content-end gap-4">
-                  <a href="#" class="icon-link"><i class="fas fa-heart"></i></a>
+                  <a href="<?php echo get_permalink(get_page_by_path('favoris')); ?>" class="icon-link"><i class="fas fa-heart"></i></a>
                   <a href="#" class="icon-link"><i class="fa-solid fa-basket-shopping"></i></a>
                   <a href="#" class="icon-link"><i class="fas fa-bell"></i></a>
                   <a href="#" class="icon-link"><img src="https://c.animaapp.com/K4rR5xEo/img/rectangle@2x.png" alt="Profile" class="icon-img rounded-circle"></a>
